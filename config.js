ec30067a9fc686cc6e9a3911b0b2b96038e5d292
Søren Gregersen <s@grgrsn.dk>
2017-01-19 19:40:49 +0100

    Added setup.js

---
+++ config.js
@@ -0,0 +1,18 @@
+var inquirer = require('inquirer');
+var fs = require('fs');
+
+runSetup();
+
+function runSetup() {
+    var questions = [
+        
+    ];
+
+    inquirer.prompt(questions).then(function (config) {
+        writeToFile(config);
+    });
+}
+
+function writeConfig(config) {
+
+}
