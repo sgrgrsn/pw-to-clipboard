7f61fd8e0d14864864c222575d7bed47c6276628
Søren Gregersen <s@grgrsn.dk>
2017-01-19 18:30:58 +0100

    First take on index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var clipboard = require('copy-paste');
+var generator = require('generate-password');
+
+var password = generator.generate({
+	length: 16,
+	numbers: true,
+	symbols: true,
+	excludeSimilarCharacters: true
+});
+
+clipboard.copy(password, function () {
+  console.log('Copied your password to clipboard');
+  console.log(password);
+});
