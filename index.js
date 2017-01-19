066d270c41a74b04b5deeeb63f742b28b3f2a966
Søren Gregersen <s@grgrsn.dk>
2017-01-19 19:41:00 +0100

    Using config from config.json

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+#!/usr/bin/env node
+
 var clipboard = require('copy-paste');
 var generator = require('generate-password');
 
+var config = require('./config.json');
+
 var password = generator.generate({
-	length: 16,
-	numbers: true,
-	symbols: true,
-	excludeSimilarCharacters: true
+	length: config['length'],
+	numbers: config['numbers'],
+	symbols: config['symbols'],
+	excludeSimilarCharacters: config['excludeSimilarCharacters']
 });
 
 clipboard.copy(password, function () {
   // Show an output message in green font color - reset the color it in the end
   console.log('\x1b[32m', 'Copied your password to clipboard 🔑', '\x1b[0m');
 
-  // Show the password in inversed colors
-  console.log(' \x1b[7m', password, '\x1b[0m');
+  if (config['showPassword']) {
+    // Show the password in inversed colors
+    console.log(' \x1b[7m', password, '\x1b[0m');
+  }
 });
